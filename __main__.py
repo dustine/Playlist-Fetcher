d133d39dca82aabb25163e34fb0c40dfaeab48d9
Dustine Camacho <dustine.camacho@gmail.com>
2017-10-05 04:20:43 +0100

    Added todo, feature... sufficient

--- __main__.py
+++ __main__.py
@@ -1,3 +1,13 @@
+import sys
+import traceback
+
 import playlist_fetcher
 
-playlist_fetcher.main()
+try:
+    playlist_fetcher.main()
+except KeyboardInterrupt:
+    print("\r\nShutdown requested...exiting", end="")
+    sys.stdout.flush()
+except Exception:
+    traceback.print_exc(file=sys.stdout)
+    sys.exit(1)
--- playlist_fetcher.py
+++ playlist_fetcher.py
@@ -1,18 +1,37 @@
 #! python3
 
+# TODO:
+'''
+√ Update stored date from newly downloaded
+√ Capture and silently deal with KeyboardInterrup
+√ The "ERROR: ERROR: ... \n\n\n" log thing -o-
+? Fix borked progress bars while downloading
+? Make flake8 know that colorama is needed
+- Make refresh check the database instead of waiting for an error?
+- Write starter script that starts venv
+- See why powershell isn't respecting \r's
+- Add option to update youtube-dl (maybe part of the init script?)
+- Purging (interactive?)
+- Respect starred bool (prevent purging, download first?)
+- Interactive starring? Removal?
+- Prettify the code (it's not very Pythonesque...)
+'''
+
 import argparse
 import copy
 import datetime
+import logging
 import os
 import pprint
 import sqlite3
 import sys
-import logging
-import string
+import re
 
 import colorama
 import youtube_dl
 from tqdm import tqdm
+
+colorama.colorama_text()
 
 # Argv Parser
 PARSER = argparse.ArgumentParser(fromfile_prefix_chars='@')
@@ -23,8 +42,11 @@ PARSER.add_argument('-r', '--refresh', action='store_true', help='refreshes data
 PARSER.add_argument('download', metavar='P', type=str, nargs='*', help='download playlists (once)')
 
 logger = logging.getLogger(__name__)
+logger.propagate = False
 handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter("%(levelname)s: %(message)s")
+
+logging.Formatter()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -36,11 +58,13 @@ class FluidStream(object):
     def __init__(self, fluid):
         self.fluid = fluid
         self.handler = logging.StreamHandler(self)
-        self.handler.setFormatter(formatter)
+        # self.handler.setFormatter(formatter)
 
-    def write(self, str):
-        self.fluid.write(str, end="\n")
-        sys.stdout.flush()
+    def write(self, string: str):
+        # print("print")
+        if string.strip():
+            self.fluid.write(string.strip())
+        # sys.stdout.flush()
 
     def flush(self):
         pass
@@ -59,30 +83,16 @@ OPTIONS = {
     'outtmpl': "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s",
     'restrictfilenames': True,
     'ignoreerrors': True,
-    # 'no_warnings': True,
+    'no_warnings': True,
     'updatetime': True,
     'quiet': True,
-    # 'postprocessors': [{
-    #     'key': 'FFmpegExtractAudio',
-    #     'preferredcodec': 'mp3',
-    #     'preferredquality': '192',
-    # }],
     'logger': logger,
-    # 'progress_hooks': [my_hook],
 }
 
 
 def init_files(path):
     """inits the hidden download files"""
     os.makedirs(path, exist_ok=True)
-
-    # conf = os.path.join(path, 'youtube-dl.conf')
-    # if not os.path.exists(conf):
-    #     with open(conf, 'w') as file:
-    #         file.write('--restrict-filenames\n'
-    #                     '--prefer-free-formats\n'
-    #                     '-o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"\n'
-    #                     '-f bestvideo[height<=?1080]+bestaudio/best[height<=?1080]/best')
 
     if not os.path.exists(os.path.join(path, 'playlists.sqlite')):
         conn = sqlite3.connect(os.path.join(path, 'playlists.sqlite'))
@@ -110,14 +120,148 @@ def get_max_upload_date(entries):
             ud = entry["upload_date"]
             # YYYYMMDD, negative quotients because YYYY can be bigger than 2 digits
             return datetime.date(int(ud[:-4]), int(ud[-4:-2]), int(ud[-2:]))
-        return datetime.date.fromtimestamp(0)
+        return None
 
-    return max(map(get_upload_date, entries), default=datetime.date.fromtimestamp(0))
+    return max(map(get_upload_date, filter(lambda x: x is not None, entries)), default=None)
 
 
 def get_id(playlist_info):
     """returns unique playlist id"""
     return "{}:{}".format(playlist_info['extractor_key'], playlist_info['id'])
+
+
+def add_playlists(database, args):
+    custom_options = copy.copy(OPTIONS)
+    custom_options["extract_flat"] = 'in_playlist'
+
+    with youtube_dl.YoutubeDL(custom_options) as ydl:
+        for playlist in args.add_playlists:
+            info = ydl.extract_info(url=playlist, download=False)
+
+            if info["_type"] != "playlist":
+                logger.warning("{} not a playlist, skipping...".format(playlist))
+            else:
+                try:
+                    database.execute("""INSERT INTO `playlists`(`id`,`url`,`title`) VALUES (?,?,?);""",
+                                     (get_id(info), info["webpage_url"], info["title"],))
+                    print("Indexed playlist {} ({}).".format(get_id(info), info["title"]))
+                except sqlite3.IntegrityError as exc:
+                    if exc.args[0] == 'UNIQUE constraint failed: playlists.id':
+                        # id must be unique, fail silently
+                        pass
+                    else:
+                        raise exc
+
+                database.commit()
+
+
+def refresh(database, args):
+    print("Refreshing database... this may take a while.")
+
+    custom_options = copy.copy(OPTIONS)
+    custom_options["youtube_include_dash_manifest"] = True
+    if 'download_archive' in OPTIONS.keys:
+        del custom_options['download_archive']
+
+    with youtube_dl.YoutubeDL(custom_options) as ydl:
+        pbar = tqdm(database.execute("""SELECT `key`, `url` FROM `playlists`""").fetchall())
+        with FluidStream(pbar):
+            for entry in pbar:
+                # print(entry)
+                info = ydl.extract_info(url=entry[1], download=False)
+
+                database.execute("""UPDATE `playlists` SET `title`=?, `date`=? WHERE `key`=?""",
+                                 (info["title"], get_max_upload_date(info["entries"]), entry[0]))
+                database.commit()
+
+
+def download(database, args):
+    indexed = database.execute("""SELECT `key`, `url` FROM `playlists` ORDER BY `date` ASC""").fetchall()
+    print("Updating {} playlists ({} indexed)...".format(len(indexed) + len(args.download), len(indexed)))
+
+    oneoffs = map(lambda elem: (None, elem), args.download)
+    custom_options = copy.copy(OPTIONS)
+    custom_options["youtube_include_dash_manifest"] = True
+
+    index_pattern = re.compile(r'^\d+')
+
+    pbar = tqdm(list(oneoffs) + indexed)
+    with FluidStream(pbar):
+        for playlist in pbar:
+            # get total videos count
+            with youtube_dl.YoutubeDL(custom_options) as ydl:
+                info = ydl.extract_info(url=playlist[1], download=False)
+                n_videos = len(info["entries"])
+
+            if n_videos <= 0:
+                continue
+
+            with tqdm(total=n_videos) as playlist_bar:
+                playlist_bar.write(info["title"])
+                video_bar = None
+                prev_size = 0
+                prev_index = -1
+
+                # callback function to report download progress
+                def report_progress(report):
+                    """youtube-dl callback function for progress"""
+                    nonlocal video_bar
+                    nonlocal prev_size
+                    nonlocal prev_index
+
+                    if report["status"] == "error":
+                        if video_bar is not None:
+                            video_bar.close()
+                        prev_size = 0
+                        playlist_bar.update()
+                    elif report["status"] == "finished":
+                        if video_bar is not None:
+                            video_bar.close()
+                        prev_size = 0
+                    elif report["status"] == "downloading":
+                        if prev_size == 0:
+                            video_bar = tqdm(total=int(9e9), position=2, unit_scale=True, unit="B")
+                            playlist_bar.refresh()
+
+                            # iterate playlist_bar with filename index
+                            match = re.match(index_pattern, os.path.basename(report["filename"]))
+                            if match is not None:
+                                new_index = int(match.group())
+                                if new_index != prev_index:
+                                    # don't interate on invalid indexes
+                                    if prev_index > 0:
+                                        playlist_bar.update()
+                                    prev_index = new_index
+                            logger.warning("Starting! " + report["filename"])
+                        if "total_bytes" in report:
+                            video_bar.total = report["total_bytes"]
+                        else:
+                            video_bar.total = report["total_bytes_estimate"]
+
+                        video_bar.update(report["downloaded_bytes"] - prev_size)
+                        prev_size = report["downloaded_bytes"]
+                    else:
+                        pprint.pprint(report)
+
+                custom_options = copy.copy(OPTIONS)
+                custom_options["progress_hooks"] = [report_progress]
+
+                with youtube_dl.YoutubeDL(custom_options) as ydl:
+                    # download the playlist
+                    info = ydl.extract_info(url=playlist[1])
+                    playlist_bar.close()
+
+                    # post-processing: save the newest upload date
+                    if playlist[0] is None:
+                        continue
+                    date = get_max_upload_date(info["entries"])
+                    if date is None:
+                        continue
+                    database.execute("""update playlists set date = coalesce(
+                        max(?, (select date from playlists where key = ?)), ?)
+                        where key = ?""",
+                                     (date, playlist[0], date, playlist[0]))
+                    database.commit()
 
 
 def main():
@@ -127,111 +271,20 @@ def main():
     database = init_files(os.path.join(path, '.playlist_fetcher'))
     archive = os.path.join(path, '.playlist_fetcher', 'archive.txt')
 
-    print(args.__dict__)
+    # print(args.__dict__)
 
-    # ignore-archive
     if args.ignore_archive is False:
-        print(archive)
         OPTIONS['download_archive'] = archive
+        # FLAT_OPTIONS and SHALLOW_OPTIONS don't use archive
 
-    flat_options = copy.copy(OPTIONS)
-    flat_options["extract_flat"] = "in_playlist"
-
-    # add-playlists
     if args.add_playlists is not None:
-        with youtube_dl.YoutubeDL(flat_options) as ydl:
-            for playlist in args.add_playlists:
-                info = ydl.extract_info(url=playlist, download=False)
+        add_playlists(database, args)
 
-                if info["_type"] != "playlist":
-                    logger.warning("{} not a playlist, skipping...".format(playlist))
-                else:
-                    try:
-                        database.execute("""INSERT INTO `playlists`(`id`,`url`,`title`) VALUES (?,?,?);""",
-                                         (get_id(info), info["webpage_url"], info["title"],))
-                        print("Indexed playlist {} ({}).".format(get_id(info), info["title"]))
-                    except sqlite3.IntegrityError as exc:
-                        if exc.args[0] == 'UNIQUE constraint failed: playlists.id':
-                            # id must be unique, fail silently
-                            pass
-                        else:
-                            raise exc
-
-            database.commit()
-
-    # refresh
     if args.refresh is True:
-        print("Refreshing database... this may take a while.")
+        refresh(database, args)
 
-        custom_options = copy.copy(OPTIONS)
-        del custom_options["download_archive"]
-        custom_options["youtube_include_dash_manifest"] = False
+    download(database, args)
 
-        with youtube_dl.YoutubeDL(custom_options) as ydl:
-            pbar = tqdm(database.execute("""SELECT `id`, `url` FROM `playlists`""").fetchall())
-            with FluidStream(pbar):
-                for entry in pbar:
-                    # print(entry)
-                    info = ydl.extract_info(url=entry[1], download=False)
-
-                    database.execute("""UPDATE `playlists` SET `title`=?, `date`=? WHERE `id`=?""",
-                                     (info["title"], get_max_upload_date(info["entries"]), entry[0]))
-                    database.commit()
-
-    # download
-    indexed = database.execute("""SELECT `id`, `url` FROM `playlists` ORDER BY `date` ASC""").fetchall()
-    print("Updating {} playlists ({} indexed)...".format(len(indexed) + len(args.download), len(indexed)))
-
-    oneoffs = map(lambda elem: (None, elem), args.download)
-    pbar = tqdm(list(oneoffs) + indexed)
-    with FluidStream(pbar):
-        for playlist in pbar:
-            with youtube_dl.YoutubeDL(flat_options) as ydl:
-                info = ydl.extract_info(url=playlist[1], download=False)
-                n_videos = len(info["entries"])
-
-            with tqdm(total=n_videos, position=1) as playlist_bar:
-                playlist_bar.write(info["title"])
-                video_bar = None
-                prev_size = 0
-
-                def report_progress(report):
-                    """youtube-dl callback function for progress"""
-                    nonlocal video_bar
-                    nonlocal prev_size
-
-                    if report["status"] == "error":
-                        if video_bar is not None:
-                            video_bar.close()
-                        prev_size = 0
-                    elif report["status"] == "finished":
-                        if video_bar is not None:
-                            video_bar.close()
-                            
-                        prev_size = 0
-                        playlist_bar.update()
-                    elif report["status"] == "downloading":
-                        if prev_size == 0:
-                            video_bar = tqdm(total=int(9e9), position=2, unit_scale=True, unit="B")
-                        if "total_bytes" in report:
-                            video_bar.total = report["total_bytes"]
-                        else:
-                            video_bar.total = report["total_bytes_estimate"]
-
-                        video_bar.update(report["downloaded_bytes"] - prev_size)
-                        prev_size = report["downloaded_bytes"]
-                    else:
-                        logger.warning(pprint.pprint(report))
-
-                progress_options = copy.copy(OPTIONS)
-                progress_options["progress_hooks"] = [report_progress]
-                progress_options["logger"] = logger
-
-                with youtube_dl.YoutubeDL(progress_options) as ydl:
-                    info = ydl.extract_info(url=playlist[1])
-                    if len(info["entries"]) > 0:
-                        with open("output_{}.txt".format(info["id"]), "w+") as file:
-                            pprint.pprint(info, stream=file)
 
 # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 #     ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
